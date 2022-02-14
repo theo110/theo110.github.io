90d3a72fa5a119e6173be62918b44b67b44baf9e
theo110 <shouyuanroh@gmail.com>
2022-02-13 21:55:16 -0500

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
     <h1>About me</h1>
     <div className="words"> 
       Hi, I'm Theo, a 1st year Mechatronics Engineering student at the University of Waterloo. I am a passionate student looking to learn
-      new exciting things about software development. Some of my hobbies include chess, playing video games, and reading about naval history.
+      new exciting things about software development. Some of my hobbies include chess, playing video games, as well as reading about history and philosophy.
       Feel free to check out my projects in the "Projects" section or in my GitHub profile.
     </div>
     </AnimatedPage>
